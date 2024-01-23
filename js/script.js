2481356b5dc5234773fd8bae6698c5a739a5891d
kasiausuaj <andrejlebed120@gmail.com>
2024-01-23 23:39:13 +0700

    изменил css + добавил немного js

--- js/script.js
+++ js/script.js
@@ -42,5 +42,32 @@ new Vue({
           console.log('Ошибка! Неправильная дата.');
         }
       },
+      editCard(card) {
+        const newTitle = prompt('Введите новое название',   card.title);
+        if (newTitle) {
+            card.title = newTitle;
+          }
+    
+          const newDescription = prompt('Введите новое описание', card.description);
+          if (newDescription) {
+            card.description = newDescription;
+          }
+    
+          card.lastEdited = new Date().toLocaleString();
+        },
+        deleteCard(card) {
+          const confirmDelete = confirm('Вы уверены, что хотите удалить карточку?');
+          if (confirmDelete) {
+            if (this.plannedTasks.includes(card)) {
+              this.plannedTasks.splice(this.plannedTasks.indexOf(card), 1);
+            } else if (this.inProgressTasks.includes(card)) {
+              this.inProgressTasks.splice(this.inProgressTasks.indexOf(card), 1);
+            } else if (this.testingTasks.includes(card)) {
+              this.testingTasks.splice(this.testingTasks.indexOf(card), 1);
+            } else if (this.completedTasks.includes(card)) {
+              this.completedTasks.splice(this.completedTasks.indexOf(card), 1);
+            }
+          }
+        },
     }
 })
