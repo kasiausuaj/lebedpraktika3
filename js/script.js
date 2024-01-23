3b36d94fb0003347be3f5ba1932aa65a0a75c142
kasiausuaj <andrejlebed120@gmail.com>
2024-01-24 01:03:14 +0700

    выполненные задачи, удаление формы,загрузка в локальное хранилище

--- js/script.js
+++ js/script.js
@@ -56,54 +56,71 @@ new Vue({
           card.lastEdited = new Date().toLocaleString();
         },
         deleteCard(card) {
-            const confirmDelete = confirm('Вы уверены, что хотите удалить карточку?');
-            if (confirmDelete) {
-              if (this.plannedTasks.includes(card)) {
-                this.plannedTasks.splice(this.plannedTasks.indexOf(card), 1);
-              } else if (this.inProgressTasks.includes(card)) {
-                this.inProgressTasks.splice(this.inProgressTasks.indexOf(card), 1);
-              } else if (this.testingTasks.includes(card)) {
+          const confirmDelete = confirm('Вы уверены, что хотите удалить карточку?');
+          if (confirmDelete) {
+            if (this.plannedTasks.includes(card)) {
+              this.plannedTasks.splice(this.plannedTasks.indexOf(card), 1);
+            } else if (this.inProgressTasks.includes(card)) {
+              this.inProgressTasks.splice(this.inProgressTasks.indexOf(card), 1);
+            } else if (this.testingTasks.includes(card)) {
+              this.testingTasks.splice(this.testingTasks.indexOf(card), 1);
+            } else if (this.completedTasks.includes(card)) {
+              this.completedTasks.splice(this.completedTasks.indexOf(card), 1);
+            }
+          }
+        },
+        moveToInProgress(card) {
+          this.plannedTasks.splice(this.plannedTasks.indexOf(card), 1);
+          this.inProgressTasks.push(card);
+        },
+        moveToTesting(card) {
+          this.inProgressTasks.splice(this.inProgressTasks.indexOf(card), 1);
+          this.testingTasks.push(card);
+        },
+        moveToCompleted(card) {
+          this.testingTasks.splice(this.testingTasks.indexOf(card), 1);
+          this.completedTasks.push(card);
+          card.completed = true;
+        },
+        returnToProgress(card) {
+            const returnReason = prompt('Введите причину возврата', '');
+            if (returnReason) {
+              card.returnReason = returnReason;
+              if (this.testingTasks.includes(card)) {
                 this.testingTasks.splice(this.testingTasks.indexOf(card), 1);
               } else if (this.completedTasks.includes(card)) {
                 this.completedTasks.splice(this.completedTasks.indexOf(card), 1);
               }
+              this.inProgressTasks.push(card);
             }
           },
-          moveToInProgress(card) {
-            this.plannedTasks.splice(this.plannedTasks.indexOf(card), 1);
-            this.inProgressTasks.push(card);
+        isDeadlineExpired(date) {
+          const deadline = new Date(date);
+          const today = new Date();
+          return deadline < today;
+        },
+        saveTasksToStorage() {
+            const tasks = {
+              plannedTasks: this.plannedTasks,
+              inProgressTasks: this.inProgressTasks,
+              testingTasks: this.testingTasks,
+              completedTasks: this.completedTasks
+            };
+            localStorage.setItem('kanbanTasks', JSON.stringify(tasks));
           },
-          moveToTesting(card) {
-            this.inProgressTasks.splice(this.inProgressTasks.indexOf(card), 1);
-            this.testingTasks.push(card);
+          loadTasksFromStorage() {
+            const tasks = JSON.parse(localStorage.getItem('kanbanTasks'));
+            if (tasks) {
+              this.plannedTasks = tasks.plannedTasks;
+              this.inProgressTasks = tasks.inProgressTasks;
+              this.testingTasks = tasks.testingTasks;
+              this.completedTasks = tasks.completedTasks;
+            }
           },
-          moveToCompleted(card) {
-            this.testingTasks.splice(this.testingTasks.indexOf(card), 1);
-            this.completedTasks.push(card);
-            card.completed = true;
-          },
-          returnToProgress(card) {
-              const returnReason = prompt('Введите причину возврата', '');
-              if (returnReason) {
-                card.returnReason = returnReason;
-                if (this.testingTasks.includes(card)) {
-                  this.testingTasks.splice(this.testingTasks.indexOf(card), 1);
-                } else if (this.completedTasks.includes(card)) {
-                  this.completedTasks.splice(this.completedTasks.indexOf(card), 1);
-                }
-                this.inProgressTasks.push(card);
-              }
-            },
-
-              saveTasksToStorage() {
-                const tasks = {
-                  plannedTasks: this.plannedTasks,
-                  inProgressTasks: this.inProgressTasks,
-                  testingTasks: this.testingTasks,
-                  completedTasks: this.completedTasks
-                };
-                localStorage.setItem('kanbanTasks', JSON.stringify(tasks));
-              },
-          
-    }
-})
+          clearForm() {
+            this.newCardTitle = '';
+            this.newCardDescription = '';
+            this.newCardDeadline = '';
+          }
+        }
+      });
